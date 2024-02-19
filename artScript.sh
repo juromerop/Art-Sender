36d73cae5a1af3775fad9911f5d012885037b37c
juromerop <juromerop@unal.edu.co>
2024-02-19 18:59:21 -0500

    done the conditional for the combination of the flags

--- artScript.sh
+++ artScript.sh
@@ -21,6 +21,18 @@ if [ -z "$fields" ] && [ -n "$search" ] && [ -z "$artworks" ]; then
     elif [ -z "$fields" ] && [ -z "$search" ] && [ -n "$artworks" ]; then
         echo "Limit for $artworks"
         curl -X GET "https://api.artic.edu/api/v1/artworks?limit=$artworks" > art.json
+    elif [ -z "$fields" ] && [ -n "$search" ] && [ -n "$artworks" ]; then
+        echo "Searching for $search and limiting to $artworks"
+        curl -X GET "https://api.artic.edu/api/v1/artworks/search?q=$search&limit=$artworks" > art.json
+    elif [ -n "$fields" ] && [ -n "$search" ] && [ -z "$artworks" ]; then
+        echo "Fields for $fields and searching for $search"
+        curl -X GET "https://api.artic.edu/api/v1/artworks/search?q=$search&fields=$fields" > art.json
+    elif [ -n "$fields" ] && [ -z "$search" ] && [ -n "$artworks" ]; then
+        echo "Fields for $fields and limiting to $artworks"
+        curl -X GET "https://api.artic.edu/api/v1/artworks?fields=$fields&limit=$artworks" > art.json
+    elif [ -n "$fields" ] && [ -n "$search" ] && [ -n "$artworks" ]; then
+        echo "Fields for $fields, searching for $search, and limiting to $artworks"
+        curl -X GET "https://api.artic.edu/api/v1/artworks/search?q=$search&fields=$fields&limit=$artworks" > art.json
     else
         echo "Invalid input"
         exit 1
