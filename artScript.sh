6c2e129016a5977ddb50a316a08621e2337ce1ca
juromerop <juromerop@unal.edu.co>
2024-02-19 18:40:43 -0500

    done the conditional for each flag

--- artScript.sh
+++ artScript.sh
@@ -15,4 +15,15 @@ done
 if [ -z "$fields" ] && [ -n "$search" ] && [ -z "$artworks" ]; then
     echo "Searching for $search"
     curl -X GET "https://api.artic.edu/api/v1/artworks/search?q=$search" > art.json
+    elif [ -n "$fields" ] && [ -z "$search" ] && [ -z "$artworks" ]; then
+        echo "Fields for $fields"
+        curl -X GET "https://api.artic.edu/api/v1/artworks?fields=$fields" > art.json
+    elif [ -z "$fields" ] && [ -z "$search" ] && [ -n "$artworks" ]; then
+        echo "Limit for $artworks"
+        curl -X GET "https://api.artic.edu/api/v1/artworks?limit=$artworks" > art.json
+    else
+        echo "Invalid input"
+        exit 1
 fi
+
+echo "Artwork data saved to art.json"
